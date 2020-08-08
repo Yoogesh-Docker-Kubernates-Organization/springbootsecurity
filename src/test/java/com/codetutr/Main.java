c994db02f96ef02bd18743e2a7532e4eb3880f7c
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-08-08 10:47:27 -0400

    Update Main.java

--- src/test/java/com/codetutr/Main.java
+++ src/test/java/com/codetutr/Main.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.util.CollectionUtils;
 
@@ -22,14 +23,29 @@ public class Main {
 		Main run = new Main();
 		
 		//run.convertHtmlFileToPDF();
-		
-		
-		System.out.println(new Date());
+		run.getDifferentTimeZoneTime();
 	}
+
 
 	private void convertHtmlFileToPDF() throws IOException, DocumentException {
 		HTMLToPDFConverter converter = new HTMLToPDFConverter();
 		converter.convertHtmlFileToPDF();
+	}
+	
+	private void getDifferentTimeZoneTime() {
+		Calendar orderSubmittedCalender = Calendar.getInstance();
+		orderSubmittedCalender.setTimeInMillis(1596549600000L - 10800000L);
+		
+		setTimeZone(orderSubmittedCalender, "America/Los_Angeles");
+		System.out.println(orderSubmittedCalender);
+		
+		setTimeZone(orderSubmittedCalender, "US_Arizona");
+		System.out.println(orderSubmittedCalender);
+	}
+	
+	private static void setTimeZone(Calendar cal, String timeZone) {
+		TimeZone tz = TimeZone.getTimeZone(timeZone);
+		cal.setTimeZone(tz);
 	}
 
 }
