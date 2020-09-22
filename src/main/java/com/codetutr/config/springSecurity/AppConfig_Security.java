39d3c9b536159a5b714bd36c22527d6eab037f25
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-09-22 13:34:12 -0400

    Update AppConfig_Security.java

--- src/main/java/com/codetutr/config/springSecurity/AppConfig_Security.java
+++ src/main/java/com/codetutr/config/springSecurity/AppConfig_Security.java
@@ -139,7 +139,7 @@ public class AppConfig_Security extends WebSecurityConfigurerAdapter{
 		http
 		.csrf().ignoringAntMatchers(ignoreCsrfForSpecificUrls);
 		
-		csrfForKubernetesCluster(http);
+		//csrfForKubernetesCluster(http);
 	}
 	
 
