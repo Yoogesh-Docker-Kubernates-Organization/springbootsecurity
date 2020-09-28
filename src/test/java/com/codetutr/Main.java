f33eaee976af56de06db9ad1bd5455bbca3ece16
yoogesh sharma <35018173+yoogesh1983@users.noreply.github.com>
2020-09-28 12:26:08 -0400

    Update Main.java

--- src/test/java/com/codetutr/Main.java
+++ src/test/java/com/codetutr/Main.java
@@ -23,7 +23,25 @@ public class Main {
 		Main run = new Main();
 		
 		//run.convertHtmlFileToPDF();
-		run.getDifferentTimeZoneTime();
+		//run.getDifferentTimeZoneTime();
+
+		Calendar orderSubmittedCalender = Calendar.getInstance();
+		orderSubmittedCalender.setTimeInMillis(1601009848000L); //1601013448000L
+		setTimeZone(orderSubmittedCalender, "US/Central");
+		//setTimeZone(orderSubmittedCalender, "America/New_York");
+		
+		System.out.println(orderSubmittedCalender.get(Calendar.DAY_OF_YEAR));
+		System.out.println(orderSubmittedCalender);
+		System.out.println(new Date(orderSubmittedCalender.getTimeInMillis()));
+		
+		Calendar orderSubmittedCalender1 = Calendar.getInstance();
+		orderSubmittedCalender1.setTimeInMillis(1601042400000L);
+		//setTimeZone(orderSubmittedCalender1, "US/Central");
+		//setTimeZone(orderSubmittedCalender1, "America/New_York");
+		
+		System.out.println(orderSubmittedCalender1.get(Calendar.DAY_OF_YEAR));
+		System.out.println(orderSubmittedCalender1);
+		System.out.println(new Date(orderSubmittedCalender1.getTimeInMillis()));
 	}
 
 
